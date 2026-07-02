e969bc0672fdd824c6dc06797d80d06fdac15130
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2026-07-02 11:12:20 +0200

    feat: Enhance IntuneUploader with new sync options and publishing state handling

--- IntuneUploader/IntuneAppCleaner.py
+++ IntuneUploader/IntuneAppCleaner.py
@@ -82,11 +82,9 @@ class IntuneAppCleaner(IntuneUploaderBase):
         # Get primaryBundleVersion or buildNumber for each app and set it as a key in the app dict
         apps = list(
             map(
-                lambda item: (
-                    {**item, "primaryBundleVersion": item["buildNumber"]}
-                    if "primaryBundleVersion" not in item and "buildNumber" in item
-                    else item
-                ),
+                lambda item: {**item, "primaryBundleVersion": item["buildNumber"]}
+                if "primaryBundleVersion" not in item and "buildNumber" in item
+                else item,
                 apps,
             )
         )
@@ -126,11 +124,11 @@ class IntuneAppCleaner(IntuneUploaderBase):
                 "keep count": str(keep_versions),
                 "match count": str(len(apps)),
                 "removed count": str(len(apps_to_delete)),
-                "removed versions": (
-                    ", ".join([app["primaryBundleVersion"] for app in apps_to_delete])
-                    if len(apps_to_delete) > 0
-                    else ""
-                ),
+                "removed versions": ", ".join(
+                    [app["primaryBundleVersion"] for app in apps_to_delete]
+                )
+                if len(apps_to_delete) > 0
+                else "",
             },
         }
 
